e1a656195db73fc4ed07af3821332156a85b62ef
Ovie Revaldi <ovierevaldi@gmail.com>
2025-06-04 09:36:51 +0700

    remove listen func

--- src/index.ts
+++ src/index.ts
@@ -32,9 +32,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
 
