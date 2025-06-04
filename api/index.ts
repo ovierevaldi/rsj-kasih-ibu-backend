485f564d97901f9928201e7678d1391d35f36b61
Ovie Revaldi <ovierevaldi@gmail.com>
2025-06-04 15:12:08 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import app from '../src/index.js';
+import app from '../src/index.js'; // or '../src/index.ts' if you're using TypeScript
 import serverless from 'serverless-http';
 
-export default serverless(app);
+export const handler = serverless(app);
