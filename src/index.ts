c3e509edbfb406917beada89a43a6e6c4b95ba5c
Ovie Revaldi <ovierevaldi@gmail.com>
2025-06-13 15:49:52 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,29 @@
 import express from 'express'
-// import type { Request, Response } from 'express';
-// import cors from 'cors';
-// import pendaftaranRoutes from './routes/pendaftaran.routes.js';
-// import dokterRoutes from './routes/dokter.routes.js';
-// import jenisPengobatanRoutes from './routes/jenis_pengobatan.routes.js';
-// import metodePembayaranRoutes from './routes/metode_pembayaran.routes.js';
-// import jadwalPengobatanRoutes from './routes/jadwal_pengobatan.route.js';
+import type { Request, Response } from 'express';
+import cors from 'cors';
+import pendaftaranRoutes from './routes/pendaftaran.routes.js';
+import dokterRoutes from './routes/dokter.routes.js';
+import jenisPengobatanRoutes from './routes/jenis_pengobatan.routes.js';
+import metodePembayaranRoutes from './routes/metode_pembayaran.routes.js';
+import jadwalPengobatanRoutes from './routes/jadwal_pengobatan.route.js';
 
 const app = express();
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.use(cors())
+app.use(cors())
 
-// app.use(express.json())
+app.use(express.json())
 
-// app.use('/pendaftaran', pendaftaranRoutes);
-// app.use('/dokter', dokterRoutes);
-// app.use('/jenis-pengobatan', jenisPengobatanRoutes);
-// app.use('/metode-pembayaran', metodePembayaranRoutes);
-// app.use('/jadwal-pengobatan', jadwalPengobatanRoutes);
+app.use('/pendaftaran', pendaftaranRoutes);
+app.use('/dokter', dokterRoutes);
+app.use('/jenis-pengobatan', jenisPengobatanRoutes);
+app.use('/metode-pembayaran', metodePembayaranRoutes);
+app.use('/jadwal-pengobatan', jadwalPengobatanRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// export const handler = serverless(app, { provider: 'azure' });
-// export const funcName = async (context: any, req: any) => {
-//   context.res = await handler(context, req);
-// };
-
-// export const handler = serverless(app);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
